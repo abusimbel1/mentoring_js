2bac178eb48d822322db345071fe6fd012385dd3
Oleksandr <Oleksandr_Hrytsiuk@epam.com>
2021-05-30 14:48:42 +0300

    sdf

--- module-1/BE/dist/routes/sayHelloWorld.js
+++ module-1/BE/dist/routes/sayHelloWorld.js
@@ -1,7 +1,10 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
-function sayHello(req, res) {
-    res.statusCode = 200;
-    res.send({ data: 'Hello' });
+// export default function sayHello(req: Request, res: Response) {
+//   res.statusCode = 200;
+//   res.send({ data: 'Hello' });
+// }
+function sayHello() {
+    return 'Hello';
 }
 exports.default = sayHello;
--- module-1/BE/src/routes/sayHelloWorld.ts
+++ module-1/BE/src/routes/sayHelloWorld.ts
@@ -1,6 +1,10 @@
 import { Request, Response } from 'express';
 
-export default function sayHello(req: Request, res: Response) {
-  res.statusCode = 200;
-  res.send({ data: 'Hello' });
+// export default function sayHello(req: Request, res: Response) {
+//   res.statusCode = 200;
+//   res.send({ data: 'Hello' });
+// }
+
+export default function sayHello() {
+  return 'Hello';
 }
